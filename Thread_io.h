280dd660c1c9a9bfc659adf92f2e1f325a5b6b14
unknown <rahma.khald2002@gmail.com>
2024-06-06 16:30:45 +0300

    task_3

--- Thread_io.h
+++ Thread_io.h
@@ -25,16 +25,16 @@ typedef struct TreeItem {
 #define BUFFER_SIZE 256
 int compareDomainCounterNames(const void *domainCounterPtr1, const void 
 *domainCounterPtr2);
-int compareInts1(const void *intPtr1, const void *intPtr2);
-void printMatrix(int *matrix, int n, int m);
-void fill_matrix(int *matrix, int sizeOfMatrix, int *query, int queryCount);
-int countNeighbors(int *matrix, int n, int m, int col, int row);
-void liveGame(int *matrix, size_t n, size_t m);
-void MedianFilter(int *matrix, int n, int m, int filterSize);
-int calculateMedianVector(int *matrix, int max_col, int row, int col, int 
-*arrayOfNums, int filter_size);
-void medianFilter(int *matrix, int filter, int n, int m);
-void parsedDomainStat(vectorVoid *data, char *start, char *end);
+    int compareInts1(const void *intPtr1, const void *intPtr2);
+    void printMatrix(int *matrix, int n, int m);
+    void fill_matrix(int *matrix, int sizeOfMatrix, int *query, int queryCount);
+    int countNeighbors(int *matrix, int n, int m, int col, int row);
+    void liveGame(int *matrix, size_t n, size_t m);
+    void MedianFilter(int *matrix, int n, int m, int filterSize);
+    int calculateMedianVector(int *matrix, int max_col, int row, int col, int 
+    *arrayOfNums, int filter_size);
+    void medianFilter(int *matrix, int filter, int n, int m);
+    void parsedDomainStat(vectorVoid *data, char *start, char *end);
 vectorVoid showVisitStats(char *stats);
 int submatricesNum(matrix *m);
 DomainCounter *subdomainVisits(char **cpdomains, int cpdomainsSize, int 
