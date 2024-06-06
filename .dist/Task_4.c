cd9d0fef3bc3c449cd85c609ed5263d93127a610
unknown <rahma.khald2002@gmail.com>
2024-06-06 16:28:05 +0300

    task_%

--- .dist/Task_4.c
+++ .dist/Task_4.c
@@ -5,10 +5,10 @@ int main() {
 \"1 intel.mail.com\", \"5 wiki.org\"]";
  vectorVoid result = showVisitStats(stats);
  // Вывод результата
- for (size_t i = 0; i < result.size; i++) {
- DomainCounter domainCounter;
- getVectorValueV(&result, i, &domainCounter);
- printf("%ld %s\n", domainCounter.counter, domainCounter.domain);
+    for (size_t i = 0; i < result.size; i++) {
+    DomainCounter domainCounter;
+    getVectorValueV(&result, i, &domainCounter);
+    printf("%ld %s\n", domainCounter.counter, domainCounter.domain);
  }
  // Очистка памяти
  deleteVectorV(&result);
